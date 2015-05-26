01faa42181099bca808b08109e6c90d7ee91f6ca
Tim Price <timprice@catalyst-au.net>
2015-05-26 14:06:07 +1000

    WR#240123 - fixing javascript bug

--- branching.js
+++ branching.js
@@ -1,23 +1,20 @@
 M.profile_field_branching = {};
 
 M.profile_field_branching.init = function(Y, fieldid, parentid, desired) {
-    parentelement = Y.one(parentid);
-    if (parentelement) {
+    if (Y.one(parentid)) {
         target = Y.one(fieldid);
         // Hide the field if we dont have the required value.
-        if (parentelement.get('value') != desired) {
-            target.setStyle('display', 'none');
+        if (Y.one(parentid).get('value') != desired) {
+            Y.one(fieldid).setStyle('display', 'none');
         }
 
-        setTimeout(function() {
-            parentelement.on('change', function(e) {
-                if (this.get('value') == desired) {
-                    target.setStyle('display', '');
-                    target.set('value', "");
-                } else {
-                    target.setStyle('display', 'none');
-                }
-            });
-        }, 200);
+        Y.one(parentid).on('change', function(e) {
+            if (this.get('value') == desired) {
+                Y.one(fieldid).setStyle('display', '');
+                Y.one(fieldid).set('value', "");
+            } else {
+                Y.one(fieldid).setStyle('display', 'none');
+            }
+        });
     }
 };
