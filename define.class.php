0ee2dfa7775176db6b8cda26b15760fea854d017
Tim Price <timprice@catalyst-au.net>
2015-08-12 11:42:49 +1000

    WR#243280 - fixing bug in options for param2

--- define.class.php
+++ define.class.php
@@ -97,10 +97,14 @@ class profile_define_branching extends profile_define_base {
 
         if ($data->param1 == USERPF_BRANCHING_CHECKLIST || $data->param1 == USERPF_BRANCHING_SECONDARY) {
 
-            $data->param2 = str_replace("\r", '', $data->param2['text']);
+            // Editors are stupid and inconsistant, so change everything to be the same.
+            $data->param2['text'] = str_replace("\r", '', $data->param2['text']);
+            $data->param2['text'] = str_replace("<p>", '', $data->param2['text']);
+            $data->param2['text'] = str_replace("</p>", '', $data->param2['text']);
+            $data->param2['text'] = str_replace("<br>", '<br />', $data->param2['text']);
 
             // Check that we have at least 2 options.
-            if (($options = explode("<br />", $data->param2)) === false) {
+            if (($options = explode("<br />", $data->param2['text'])) === false) {
                 $err['param2'] = get_string('profilemenunooptions', 'admin');
             } else if (count($options) < 2) {
                 $err['param2'] = get_string('profilemenutoofewoptions', 'admin');
