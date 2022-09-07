13d16a16419598386f60496bf2d85114f5f4fcc2
Peter Burnett <peterburnett@catalyst-au.net>
2022-09-08 08:58:08 +1000

    Convert CI to reusable workflows

--- field.class.php
+++ field.class.php
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-use engage_survey\result\option;
-
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/user/profile/field/branching/locallib.php');
